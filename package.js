f7e8825fbf5dc9c7953b779db79ab8eaf0b5f66d
Elliott Spira <elliottspira@gmail.com>
2015-05-19 15:57:25 +1000

    updating to fix scope of exported rollbar namespace

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'saucecode:rollbar',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'Rollbar error reporting integrations for Meteor',
   documentation: 'README.md'
 });
