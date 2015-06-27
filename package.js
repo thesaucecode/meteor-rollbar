f7ac1aca8c485dc5a97ce32c38cf6a989f03dfa8
Rohan <rjerrems@gmail.com>
2015-06-27 22:15:42 +1000

    Increment version number for the addition of latest client side js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'saucecode:rollbar',
-  version: '0.0.6',
+  version: '0.0.7',
   summary: 'Rollbar error reporting integrations for Meteor',
   documentation: 'README.md'
 });
